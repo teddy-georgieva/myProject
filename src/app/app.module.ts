d59011525f912ce4df88b9e1e1bf06566486c280
Teodora Georgieva <teodora.georgieva@orbisagency.com>
2022-09-15 10:41:07 +0300

    create a table.component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,18 +6,22 @@ import { MatInputModule } from '@angular/material/input';
 import { MatRadioModule } from '@angular/material/radio';
 import { MatExpansionModule } from '@angular/material/expansion';
 import {MatButtonModule} from '@angular/material/button';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import { MatTableModule } from '@angular/material/table';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { FormPageComponent } from './pages/form-page/form-page.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TableComponent } from './component/table/table.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomePageComponent,
-    FormPageComponent
+    FormPageComponent,
+    TableComponent,
   ],
   imports: [
     BrowserModule,
@@ -30,6 +34,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     MatRadioModule,
     MatExpansionModule,
     MatButtonModule,
+    MatDatepickerModule,
+    MatTableModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
